fb9d4aaca7d9f3b88d640a0717a85174a194cbc0
Eliana Tejeda <etejeda113@gmail.com>
2026-03-14 15:15:25 -0500

    Lights turn on

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,6 +36,18 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+#define T0H_CYCLES   24
+#define T0L_CYCLES   76
+#define T1H_CYCLES   64
+#define T1L_CYCLES   36
+#define RES_CYCLES   22400
+
+#define LED_PIN     GPIO_PIN_6
+#define LED_PORT    GPIOA
+
+#define LED_HIGH()  (LED_PORT->BSRR = LED_PIN)
+#define LED_LOW()   (LED_PORT->BSRR = (LED_PIN << 16)) // reset
+
 
 /* USER CODE END PM */
 
@@ -49,6 +61,58 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
+
+void DWT_Init(void)
+{
+CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;// enable trace
+DWT->CYCCNT = 0;// reset counter
+DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;// enable cycle counter
+}
+
+static inline void delay_cycles(uint32_t cycles)
+{
+uint32_t start = DWT->CYCCNT;
+while((DWT->CYCCNT - start) < cycles);
+}
+
+static inline void send_bit(uint8_t bit)
+{
+    if(bit)
+    {
+        LED_HIGH();
+        delay_cycles(T1H_CYCLES);
+        LED_LOW();
+        delay_cycles(T1L_CYCLES);
+    }
+    else
+    {
+        LED_HIGH();
+        delay_cycles(T0H_CYCLES);
+        LED_LOW();
+        delay_cycles(T0L_CYCLES);
+    }
+}
+
+void send_led(uint8_t red, uint8_t green, uint8_t blue, uint8_t white)
+{
+    uint32_t color = 
+        ((uint32_t)red << 24) |
+        ((uint32_t)green   << 16) |
+        ((uint32_t)blue  << 8)  |
+        ((uint32_t)white);
+
+    for(int8_t i = 31; i >= 0; i--)
+    {
+        send_bit((color >> i) & 1);
+    }
+}
+
+void send_reset(void)
+{
+    LED_LOW();
+    delay_cycles(RES_CYCLES);
+}
+
 
 /* USER CODE END PFP */
 
@@ -74,7 +138,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  DWT_Init();
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -95,19 +159,40 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
-    HAL_Delay(100);
-
-
-
 
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 
+
+    // HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+    // delay_cycles(20000000);
+    // HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
+    // delay_cycles(20000000);  }
+
+        __disable_irq();
+
+        send_led(0x00, 0xFF, 0x00, 0x00); // Green
+        send_reset();
+
+        __enable_irq();
+
+        HAL_Delay(500);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+
+
+        __disable_irq();
+
+        send_led(0xFF, 0x00, 0x00, 0x00); // Red
+        send_reset();
+
+        __enable_irq();
+
+        HAL_Delay(500);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
+
+
+    /* USER CODE END 3 */
+}
+}
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -131,7 +216,13 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
   RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -141,12 +232,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
